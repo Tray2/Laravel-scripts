d9cca49cef84ca3d7c4d6c844818bc94719d8c19
Tray2 <tray2@tray2.se>
2022-09-10 09:08:32 +0200

    Add fropping of PostgreSql databases

--- klp.sh
+++ klp.sh
@@ -5,16 +5,34 @@
     cd "$HOME"/code
   }
 
-  function dropDatabase() {
+  function dropMySQLDatabase() {
     readonly DATABASE_NAME=${1//[-]/_}
     readonly DATABASE_NAME_TEST=${1//[-]/_}_test
-    readonly MYSQL=`which mysql`
     readonly Q1="DROP DATABASE IF EXISTS $DATABASE_NAME;"
     readonly Q2="DROP DATABASE IF EXISTS $DATABASE_NAME_TEST;"
-    echo 'Dropping Databases.'
-    $MYSQL -uroot -e "$Q1"
-    $MYSQL -uroot -e "$Q2"
-    echo 'Databases Dropped.'
+    readonly MYSQL=`which mysql`
+    if [ ! $MYSQL = 'mysql not found' ]
+    then 
+      echo 'Dropping Databases.'
+      $MYSQL -uroot -e "$Q1"
+      $MYSQL -uroot -e "$Q2"
+      echo 'Databases Dropped.'
+    fi
+  }
+
+  function dropPostgreDatabase() {
+    readonly DATABASE_NAME=${1//[-]/_}
+    readonly DATABASE_NAME_TEST=${1//[-]/_}_test
+    readonly Q1="DROP DATABASE $DATABASE_NAME;"
+    readonly Q2="DROP DATABASE $DATABASE_NAME_TEST;"
+    readonly PSQL=`which psql`
+    if [ ! $PSQL = 'psql not found' ]
+    then
+      echo 'Dropping Databases.'
+      $PSQL postgres -c "$Q1" > /dev/null 2>&1
+      $PSQL postgres -c "$Q2" > /dev/null 2>&1
+      echo 'Databases Dropped.'
+    fi
   }
 
   function deleteProject() {
@@ -28,7 +46,8 @@
   then
     echo 'Remove Laravel project.' 
     exitProject
-    dropDatabase $1
+    dropMySQLDatabase $1
+    dropPostgreDatabase $1
     deleteProject $1
     echo 'Laravel Project Removed.'
   else
